96920c803b5e879fed4d60997bee1174a310631e
hanul-cha <ckgks3014@naver.com>
2021-08-11 15:45:07 +0900

    state

--- src/App.js
+++ src/App.js
@@ -2,39 +2,21 @@ import React from "react";
 
 //밑에 컴포넌트는 함수가 아니기대문에 return이 없어서 render를 사용해줘야함
 class App extends React.Component{ //리엑트 컴포넌트는 render()메서드를 가지고 있음 extands로 상속 받아왔기때문에  이제 App컴포넌트에서도 redner를 사용가능함
-  constructor(props) {
-    super(props);
-    console.log("hellow");
-  }
   state = {
-    count: 0,
-  }
-  add = () => {
-    this.setState(current => ({ count: current.count +1 }));
+    isLoading: true,
+    movies: [],
   };
-  minus = () => {
-    this.setState(current => ({ count: current.count -1 }));
-  };
-/* 
-  이부분에도 state를 불러 사용할수 있지만 작동하지않는다 그래서 새스테이츠를 만들어줘야함
-  setState를 호출하게 되면(setState를 호출할때 마다) react는 밑에 state를 refresh(새로고침)하고 render함수를 호출해줄것임
-*/
   componentDidMount() {
-    console.log("component")
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
-  componentDidUpdate() {
-    console.log("I just Update")
-  }
+
   render(){
-    console.log("I am rendering");
-    return (
-      <div>
-        <h1> number is : {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>minus</button>
-      </div>
-    )
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loding...": "We are ready"}</div>;
   }
+
 }
 
 export default App;
