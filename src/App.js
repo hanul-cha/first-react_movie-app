6081df01669fadb520fb39abf4abd85e71dc8e51
hanul-cha <ckgks3014@naver.com>
2021-08-12 16:53:01 +0900

    axios for fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 //밑에 컴포넌트는 함수가 아니기대문에 return이 없어서 render를 사용해줘야함
 class App extends React.Component{ //리엑트 컴포넌트는 render()메서드를 가지고 있음 extands로 상속 받아왔기때문에  이제 App컴포넌트에서도 redner를 사용가능함
@@ -6,10 +7,14 @@ class App extends React.Component{ //리엑트 컴포넌트는 render()메서드
     isLoading: true,
     movies: [],
   };
+
+  //https://yts-proxy.now.sh/list_movies.json
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    console.log(movies);
+  }
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovies();
   }
 
   render(){
