9ffe5271b57327784276619c6deea5f2f4e4900d
hanul-cha <ckgks3014@naver.com>
2021-08-12 17:50:05 +0900

    create conponent to movies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
 //밑에 컴포넌트는 함수가 아니기대문에 return이 없어서 render를 사용해줘야함
 class App extends React.Component{ //리엑트 컴포넌트는 render()메서드를 가지고 있음 extands로 상속 받아왔기때문에  이제 App컴포넌트에서도 redner를 사용가능함
@@ -14,16 +15,26 @@ class App extends React.Component{ //리엑트 컴포넌트는 render()메서드
       data: {
         data: { movies }
       }
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-    console.log(movies);
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
   }
   componentDidMount() {
     this.getMovies();
   }
 
   render(){
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loding...": "We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return <div>{isLoading ? "Loding...": movies.map(movies => ( 
+        <Movie 
+          key={movies.id}
+          id={movies.id} 
+          year={movies.year} 
+          title={movies.title} 
+          summary={movies.summary} 
+          poster={movies.medium_cover_image} 
+        />
+      )
+    )}</div>;
   }
 
 }
